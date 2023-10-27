05c126a9059828dc5761a5db61b421309e0d89e1
Siyu <s.wang163@lse.ac.uk>
2023-10-27 17:59:12 +0100

    Initial function for data transformation of iris dataset

---
+++ exercise1_assignment2.R
@@ -0,0 +1,15 @@
+# The following code creates a function named species_avg_sepal_length, 
+# which will return the average sepal length for a specified iris species.
+# The built-in iris dataset will be used, which contains measurements for 150 iris flowers from three different species.
+
+library(dplyr)
+
+species_avg_sepal_length <- function(data, species_name) {
+  species_data<-data %>% filter(Species == species_name)
+  return(mean(species_data$Sepal.Length))
+}
+
+# Example usage:
+avg_length <- species_avg_sepal_length(iris, "versicolor")
+print(avg_length)
+
